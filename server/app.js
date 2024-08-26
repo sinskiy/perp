37d3feb64ca1c692b9047df5042d8c715fb336df
sinskiy <sinskiy@proton.me>
2024-08-26 17:56:21 +0300

    feat: add errorHandler

--- server/app.js
+++ server/app.js
@@ -1,12 +1,15 @@
 import express, { json, urlencoded } from "express";
 import cors from "cors";
 import router from "./routes";
+import errorHandler from "./middleware/errorHandler";
 const app = express();
 app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
 app.use("/api", router);
+
+app.use(errorHandler);
 
 app.listen(process.env.PORT, () => {
   if (process.env.NODE_ENV === "development") {

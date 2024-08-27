ba5697087232cc68c845933fcb337a5f8fe7e721
sinskiy <sinskiy@proton.me>
2024-08-27 09:22:32 +0300

    feat(client): add static sign up page

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,9 +1,16 @@
 import Root from "./routes/Root";
+import Signup from "./routes/Signup";
 
 const routes = [
   {
     path: "/",
     element: <Root />,
+    children: [
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+    ],
   },
 ];
 
--- client/src/routes/Root.jsx
+++ client/src/routes/Root.jsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 function Root() {
   return (
     <>
+      <h1>hello world</h1>
       <main>
         <Outlet />
       </main>

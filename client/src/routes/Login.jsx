64bd0e3f9c49706e9e1fb2f2ab66d5eebf6f829a
sinskiy <sinskiy@proton.me>
2024-08-27 10:42:14 +0300

    feat(client): log in

--- client/src/routes/Login.jsx
+++ client/src/routes/Login.jsx
@@ -1,11 +1,37 @@
+import { useNavigate } from "react-router-dom";
 import Form from "../components/Form";
 import InputField from "../components/InputField";
+import useFetch from "../hooks/useFetch";
+import { useEffect } from "react";
 
 export default function Login() {
+  const { data, error, isLoading, fire } = useFetch("post");
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (data) {
+      localStorage.setItem("token", data.token);
+      return navigate("/");
+    }
+  }, [data]);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const data = new FormData(event.target);
+    fire("/auth/login", {
+      body: JSON.stringify({
+        username: data.get("username"),
+        password: data.get("password"),
+      }),
+    });
+  }
+  console.log(data, error, isLoading);
   return (
     <div className="centered-section">
       <h1>log in</h1>
-      <Form>
+      <Form isLoading={isLoading} method="post" onSubmit={handleSubmit}>
+        {error && <p aria-live="polite">{error}</p>}
         <InputField label="username" required />
         <InputField label="password" type="password" required />
       </Form>

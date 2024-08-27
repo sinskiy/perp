5503cf25faeb0b88eb1c20c23464c8993d316e02
sinskiy <sinskiy@proton.me>
2024-08-27 08:57:36 +0300

    fix(client): make testing work properly

--- client/vitest.setup.js
+++ client/vitest.setup.js
@@ -1,6 +1,9 @@
 import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
-import { afterEach } from "vitest";
+import { afterEach, expect } from "vitest";
+
+import * as matchers from "@testing-library/jest-dom/matchers";
+expect.extend(matchers);
 
 afterEach(() => {
   cleanup();

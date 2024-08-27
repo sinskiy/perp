45383f8fe95ad10fa1f3cccb76371b3a0185944b
sinskiy <sinskiy@proton.me>
2024-08-27 09:12:49 +0300

    feat(client): add basic styling

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./reset.css";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes";

749c5289f7fd474524093bb29ce33bd0307e647c
sinskiy <sinskiy@proton.me>
2024-09-04 19:24:17 +0300

    fix(client): log out on reload

--- client/src/context/UserContext.tsx
+++ client/src/context/UserContext.tsx
@@ -29,7 +29,7 @@ const Context = ({ children }: PropsWithChildren) => {
   const { data, fetchData } = useFetch();
 
   useEffect(() => {
-    fetchData("/auth");
+    fetchData("/auth", { credentials: "include" });
   }, []);
 
   useEffect(() => {
